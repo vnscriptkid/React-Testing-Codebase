d52aa06c71e1e31963c32fb114cfaca834c011f8
Thanh Nguyen <vnscriptkid@gmail.com>
2019-03-26 20:10:18 +0700

    Updates

---
+++ precache-manifest.9bae4639df9a07467dadd7f36e08c487.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd608476bb004637e094",
+    "url": "/React-Testing-Codebase/static/js/runtime~main.3746ab83.js"
+  },
+  {
+    "revision": "e75954ab53b3461c2204",
+    "url": "/React-Testing-Codebase/static/js/main.2b169cb6.chunk.js"
+  },
+  {
+    "revision": "e32f4eab910f27f5dcd2",
+    "url": "/React-Testing-Codebase/static/js/2.c654986a.chunk.js"
+  },
+  {
+    "revision": "fc43f8e58c8b0c7e3d6b34253cdd91dc",
+    "url": "/React-Testing-Codebase/index.html"
+  }
+];

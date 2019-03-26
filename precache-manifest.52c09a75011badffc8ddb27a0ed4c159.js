2b9e775c30700392fb4b3595999842ac3a590be7
Thanh Nguyen <vnscriptkid@gmail.com>
2019-03-26 17:35:14 +0700

    Updates

---
+++ precache-manifest.52c09a75011badffc8ddb27a0ed4c159.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd608476bb004637e094",
+    "url": "/React-Testing-Codebase/static/js/runtime~main.3746ab83.js"
+  },
+  {
+    "revision": "be01364a4fa3c15fb881",
+    "url": "/React-Testing-Codebase/static/js/main.677018a7.chunk.js"
+  },
+  {
+    "revision": "e32f4eab910f27f5dcd2",
+    "url": "/React-Testing-Codebase/static/js/2.c654986a.chunk.js"
+  },
+  {
+    "revision": "b35134784a2c2aad1e3bd154052cbed4",
+    "url": "/React-Testing-Codebase/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.052c3f78a839abebd2807ac09d6cba48.js"
+  "/React-Testing-Codebase/precache-manifest.52c09a75011badffc8ddb27a0ed4c159.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Testing-Codebase/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
